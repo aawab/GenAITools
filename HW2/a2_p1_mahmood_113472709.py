6325b89364d8d232bf024cd17e124a7e2a5a88e3
unknown <aawabmahmood2002@gmail.com>
2025-03-18 23:01:56 -0400

    HW2 setup

---
+++ HW2/a2_p1_mahmood_113472709.py
@@ -0,0 +1,18 @@
+# import random, os, sys, math, csv, re, collections, string
+# import numpy as np
+# import csv #(in-built and lightweight!)
+# import torch
+# from torch import nn, Tensor
+# import torch.nn.functional as F
+
+# import transformers
+# from sklearn.model_selection import train_test_split
+# from sklearn.metrics import f1_score
+
+# import heapq
+# import matplotlib
+
+
+
+if __name__ == "__main__":
+    print("Hello World")
