49a4fe3844456ab2acfea56cf245d06b8dae277f
unknown <aawabmahmood2002@gmail.com>
2025-04-06 23:02:38 -0400

    hw3 files

---
+++ HW3/a3_p2_mahmood_113472709.py
@@ -0,0 +1,19 @@
+import os, sys, random, re, collections, string
+import numpy as np
+import torch
+
+import math
+import csv
+
+import sklearn.model_selection
+import sklearn.metrics
+
+import heapq
+import matplotlib
+import tqdm
+
+import transformers
+import datasets
+import sentence_transformers
+
+# You may now use any AutoModel for getting the LMs. You are welcome to reference any examples from Hugging Face themselves.
